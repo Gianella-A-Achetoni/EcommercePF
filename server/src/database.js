8664cd068b17f0bffa206822378d8cc47aae572c
Gianella Ariadna Achetoni <135769239+Gianella-A-Achetoni@users.noreply.github.com>
2024-11-10 18:21:33 -0300

    Update database.js

--- server/src/database.js
+++ server/src/database.js
@@ -1,15 +1,15 @@
 import 'dotenv/config';
 import { Sequelize } from 'sequelize';
 
-console.log('dotenv loaded:', !!process.env.DB_USER);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
+console.log('dotenv loaded:', !!'root');
+console.log('DB_USER:', 'root');
+console.log('DB_PASSWORD:', 'AJtsoUoepcoZFdpavUZMsxfDkdzngUMf');
+console.log('DB_NAME:', 'railway');
+console.log('DB_HOST:', 'mysql.railway.internal');
+console.log('DB_PORT:', 3306);
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize('railway', 'root', 'AJtsoUoepcoZFdpavUZMsxfDkdzngUMf', {
+  host: 'mysql.railway.internal',
   dialect: 'mysql',
 });
 
