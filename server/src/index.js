58c20a8f6233314c2aec3a04b2f2908dbb480007
Gianella A. Achetoni <gianellaachetoni@gmail.com>
2024-11-10 19:34:40 -0300

    u

--- server/src/index.js
+++ server/src/index.js
@@ -25,7 +25,7 @@ const client = new MercadoPagoConfig({
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'client', 'HTML')));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'HTML', 'index.html'));
