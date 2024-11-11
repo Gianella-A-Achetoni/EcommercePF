f1d5e67370f7bc786d470606aa58a689be4fdd01
Gianella A. Achetoni <gianellaachetoni@gmail.com>
2024-11-10 22:21:48 -0300

    uyu

--- server/src/preload/preloadUser.js
+++ server/src/preload/preloadUser.js
@@ -7,48 +7,6 @@ const users = [
         edad: 19,
         useremail: 'gianellaachetoni@gmail.com'
     },
-    {
-        username: 'ezequiel',
-        userpassword: '123456',
-        edad: 36,
-        useremail: 'gianellaachetoni@gmail.com'
-    },
-    {
-        username: 'jessica',
-        userpassword: '123456',
-        edad: 30,
-        useremail: 'gianellaachetoni@gmail.com'
-    },
-    {
-        username: 'sofi',
-        userpassword: '123456',
-        edad: 27,
-        useremail: 'gianellaachetoni@gmail.com'
-    },
-    {
-        username: 'gianellaachetoni@gmail.com',
-        userpassword: 'n4*EDnQ3r+7YAMX',
-        edad: 19,
-        useremail: 'gianellaachetoni@gmail.com'
-    },
-    {
-        username: 'gianellaachetoni@gmail.com',
-        userpassword: 'n4*EDnQ3r+7YAMX',
-        edad: 19,
-        useremail: 'gianellaachetoni@gmail.com'
-    },
-    {
-        username: 'gianellaachetoni@gmail.com',
-        userpassword: 'Ga142536475869',
-        edad: 19,
-        useremail: 'gianellaachetoni@gmail.com'
-    },
-    {
-        username: 'gianellaachetoni@gmail.com',
-        userpassword: '12345678',
-        edad: 19,
-        useremail: 'gianellaachetoni@gmail.com'
-    }
 ];
 
 const preloadUser = async () => {
